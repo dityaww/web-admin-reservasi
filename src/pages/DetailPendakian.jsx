646e2bcc8525609b7d3292d7ff1bdd4dc74b01ab
dityaww <111202012838@mhs.dinus.ac.id>
2024-01-30 21:00:56 +0700

    update detail-pendakian

--- src/pages/DetailPendakian.jsx
+++ src/pages/DetailPendakian.jsx
@@ -5,13 +5,24 @@ import { detailPendakian } from '../api/api'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import moment from 'moment';
+import Lottie from 'react-lottie';
+import animationData from '../assets/load-data.json';
 
 function DetailPendakian() {
     const { id } = useParams()
     const token = localStorage.getItem('token')
 
-    const [dataDetail, setDataDetail] = useState()
+    const [dataDetail, setDataDetail] = useState(null)
     const [dataPendaki, setDataPendaki] = useState([])
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        },
+      };
 
     // var set = moment
 
@@ -50,55 +61,62 @@ function DetailPendakian() {
                     </div>
                 </div>
 
-                <div className="border border-gray-300 px-14 py-10 my-8 rounded-lg flex flex-col gap-4 relative">
-                    <div className={`absolute top-0 right-0 ${dataDetail?.status_pembayaran === 'success' ? 'bg-emerald-500' : 'bg-gray-300'} px-12 py-2 rounded-full m-8`}>
-                        <p className={`text-lg font-semibold ${dataDetail?.status_pembayaran === 'success' ? 'text-white' : 'text-gray-500'}`}>{dataDetail?.status_pembayaran}</p>
+                {dataDetail === null ? (
+                    <div className='flex items-center justify-center'>
+                        <Lottie options={defaultOptions} height={300} width={300} />
                     </div>
-                    <div className="flex flex-col gap-2">
-                        <p className='text-4xl font-bold text-gray-800'>Gunung Ungaran</p>
-                        <p className='text-gray-600 text-lg'>via Mawar Camp Area</p>
-                    </div>
-                    <div className="flex flex-row gap-5">
-                        <p className='border border-indigo-500 text-indigo-600 px-6 py-2 rounded-md font-bold'>{dataDetail?.jumlah_pendaki} pendaki</p>
-                        <p className='border border-emerald-500 text-emerald-600 px-6 py-2 rounded-md font-bold'>{dataDetail?.durasi_pendakian}</p>
-                    </div>
-                    <div className="flex flex-col gap-2 mt-3">
-                        <p className='text-xl font-semibold text-gray-800'>Check In</p>
-                        <p>{moment(dataDetail?.check_in).locale('id').format("LLL")}</p>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <p className='text-xl font-semibold text-gray-800'>Check Out</p>
-                        <p>{moment(dataDetail?.check_out).locale('id').format("LLL")}</p>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <p className='text-xl font-semibold text-gray-800'>Tanggal Pendakian</p>
-                        <p>{dataDetail?.tanggal_pendakian}</p>
-                    </div>
+                ) : (
+                    <div className="border border-gray-300 px-14 py-10 my-8 rounded-lg flex flex-col gap-4 relative">
+                        <div className={`absolute top-0 right-0 ${dataDetail?.status_pembayaran === 'success' ? 'bg-emerald-500' : 'bg-gray-300'} px-12 py-2 rounded-full m-8`}>
+                            <p className={`text-lg font-semibold ${dataDetail?.status_pembayaran === 'success' ? 'text-white' : 'text-gray-500'}`}>{dataDetail?.status_pembayaran}</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <p className='text-4xl font-bold text-gray-800'>Gunung Ungaran</p>
+                            <p className='text-gray-600 text-lg'>via Mawar Camp Area</p>
+                        </div>
+                        <div className="flex flex-row gap-5">
+                            <p className='border border-indigo-500 text-indigo-600 px-6 py-2 rounded-md font-bold'>{dataDetail?.jumlah_pendaki} pendaki</p>
+                            <p className='border border-emerald-500 text-emerald-600 px-6 py-2 rounded-md font-bold'>{dataDetail?.durasi_pendakian}</p>
+                        </div>
+                        <div className="flex flex-col gap-2 mt-3">
+                            <p className='text-xl font-semibold text-gray-800'>Check In</p>
+                            <p>{moment(dataDetail?.check_in).locale('id').format("LLL")}</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <p className='text-xl font-semibold text-gray-800'>Check Out</p>
+                            <p>{moment(dataDetail?.check_out).locale('id').format("LLL")}</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <p className='text-xl font-semibold text-gray-800'>Tanggal Pendakian</p>
+                            <p>{dataDetail?.tanggal_pendakian}</p>
+                        </div>
 
-                    <p className='text-xl font-semibold text-gray-800'>Anggota Pendaki</p>
-                    <div className="grid grid-cols-3 gap-4">
-                        {dataPendaki.length !== 0 && dataPendaki.map((items) => (
-                            <div className="relative border border-gray-300 shadow-md p-6 rounded-lg">
-                                <p className='bg-blue-500 absolute right-0 top-0 m-2 rounded-full px-4 py-1 text-white font-bol text-sm'>{items.code}</p>
-                                <p className='capitalize text-lg font-bold'>{items.name}</p>
-                                <p className='text-gray-500 font-semibold'>{items.email}</p>
-                                <p className='text-gray-500'>{items.phone === 'false' ? '-' : items.phone}</p>
-                                <p className='text-gray-500'>{items.address === 'false' ? '-' : items.address}</p>
-                            </div>
-                        ))}
-                    </div>
+                        <p className='text-xl font-semibold text-gray-800'>Anggota Pendaki</p>
+                        <div className="grid grid-cols-3 gap-4">
+                            {dataPendaki.length !== 0 && dataPendaki.map((items) => (
+                                <div className="relative border border-gray-300 shadow-md p-6 rounded-lg">
+                                    <p className='bg-blue-500 absolute right-0 top-0 m-2 rounded-full px-4 py-1 text-white font-bol text-sm'>{items.code}</p>
+                                    <p className='capitalize text-lg font-bold'>{items.name}</p>
+                                    <p className='text-gray-500 font-semibold'>{items.email}</p>
+                                    <p className='text-gray-500'>{items.phone === 'false' ? '-' : items.phone}</p>
+                                    <p className='text-gray-500'>{items.address === 'false' ? '-' : items.address}</p>
+                                </div>
+                            ))}
+                        </div>
 
-                    <div className="flex flex-row justify-between bg-gray-600 rounded-lg p-10 mt-14">
-                        <p className='italic text-xl text-white font-bold'>Subtotal</p>
-                        <p className='font-bold text-white text-xl'>{
-                            new Intl.NumberFormat('id-ID', {
-                                style: 'currency',
-                                currency: 'IDR',
-                                minimumFractionDigits: 0,
-                            }).format(dataDetail?.total)
-                        }</p>
+                        <div className="flex flex-row justify-between bg-gray-600 rounded-lg p-10 mt-14">
+                            <p className='italic text-xl text-white font-bold'>Subtotal</p>
+                            <p className='font-bold text-white text-xl'>{
+                                new Intl.NumberFormat('id-ID', {
+                                    style: 'currency',
+                                    currency: 'IDR',
+                                    minimumFractionDigits: 0,
+                                }).format(dataDetail?.total)
+                            }</p>
+                        </div>
                     </div>
-                </div>
+                )}
+
             </div>
 
         </section>
